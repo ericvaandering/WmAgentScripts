f02aeef1f5d45d3910c857f785279eaec4634f75
Eric Vaandering <ericvaandering@gmail.com>
2014-04-14 16:58:03 -0500

    New varaibles

--- GatherStats.py
+++ GatherStats.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+import copy
 import json
 import pdb
 import pprint
@@ -7,6 +8,16 @@ import time
 
 from WMCoreService.WMStatsClient import WMStatsClient
 from WMCoreService.DataStruct.RequestInfoCollection import RequestInfoCollection
+
+
+
+
+#def conditionalUpdate(theDict, updateDict, theKey, theValue, updateKey):
+    #if not theDict.get(theKey, None):
+        #theDict.update(theKey, theValue)
+        #updateDict[updateKey] = True
+
+    #return
 
 if __name__ == "__main__":
 
@@ -27,6 +38,9 @@ if __name__ == "__main__":
     result = requestCollection.getJSONData()
 
     for wf in result.keys():
+
+        oldRecord = copy.deepcopy(report.get(wf, None))
+
         nJobs = requests[wf].get('total_jobs', 0)
         priority = requests[wf]['priority']
         requestType = requests[wf]['request_type']
@@ -38,16 +52,35 @@ if __name__ == "__main__":
             successJobs += jobs['sucess']
             totalJobs += jobs['created']
 
+        if totalJobs and not report[wf].get('firstJobTime', None):
+            firstJobTime = time.time()
+            report[wf].update({'firstJobTime':firstJobTime,})
+        if totalJobs and successJobs == totalJobs and not report[wf].get('lastJobTime', None):
+            report[wf].update({'lastJobTime' : time.time()})
+
+
         startTime = None
         endTime = None
         complete90 = None
         finalStatus = None
+        acquireTime = None
+        closeoutTime = None
+        completedTime = None
+        announcedTime = None
         for status in requests[wf]['request_status']:
             finalStatus = status['status']
+            if status['status'] == 'completed':
+                completedTime = status['update_time']
             if status['status'] == 'acquired':
                 startTime = status['update_time']
+                acquireTime = status['update_time']
             if status['status']	== 'closed-out':
                 endTime = status['update_time']
+                closeoutTime = status['update_time']
+            if status['status']	== 'announced':
+                announcedTime = status['update_time']
+
+
 
         if totalJobs > 0:
             percentDone = (successJobs / totalJobs)*100.0
@@ -57,6 +90,15 @@ if __name__ == "__main__":
         newValue = report.setdefault(wf, {})
         if newValue:
             updatedReport[wf] = True
+
+        if acquireTime and not report[wf].get('acquireTime', None):
+            report[wf].update({'acquireTime':acquireTime})
+        if closeoutTime and not report[wf].get('closeoutTime', None):
+            report[wf].update({'closeoutTime':closeoutTime})
+        if announcedTime and not report[wf].get('announcedTime', None):
+            report[wf].update({'announcedTime':announcedTime})
+        if completedTime and not report[wf].get('completedTime', None):
+            report[wf].update({'completedTime':completedTime})
 
         report[wf].update({'priority':priority, 'percent':percentDone, 'start':startTime,
                            'end':endTime, 'totalJobs':totalJobs, 'status':finalStatus,
@@ -70,6 +112,10 @@ if __name__ == "__main__":
                 print "Added %s%% for workflow %s" % (percentage, wf)
                 updatedReport[wf] = True
                 report[wf]['percents'][percentage] = time.time()
+
+        newRecord = report[wf]
+        if oldRecord != newRecord:
+            print "Workflow updated: ", wf
 
     print "\ntotal %s requests retrieved" % len(result)
 
