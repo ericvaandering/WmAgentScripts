d4edc4ddc2f9257b8988d1631a4e8d1cd841eee1
Eric Vaandering <ericvaandering@gmail.com>
2014-04-15 09:18:35 -0500

    Return empty list on KeyError of AgentJobInfo

--- WMCoreService/DataStruct/RequestInfoCollection.py
+++ WMCoreService/DataStruct/RequestInfoCollection.py
@@ -1,5 +1,5 @@
 class JobSummary(object):
-    
+
     def __init__(self , jobStatus = None):
         self.jobStatus = {
                  "success": 0,
@@ -14,9 +14,9 @@ class JobSummary(object):
          }
         if jobStatus != None:
             self.addJobStatusInfo(jobStatus)
-    
+
     def addJobStatusInfo(self, jobStatus):
-        
+
         #TODO need to validate the structure.
         for key, value in self.jobStatus.items():
             if type(value) == int:
@@ -25,8 +25,8 @@ class JobSummary(object):
                 for secondKey, secondValue in value.items():
                     if jobStatus.has_key(key) and jobStatus[key].has_key(secondKey):
                         self.jobStatus[key][secondKey] += jobStatus[key][secondKey]
-                    
-                    
+
+
     def getTotalJobs(self):
         return (self.getSuccess() +
                 self.jobStatus["canceled"] +
@@ -37,40 +37,40 @@ class JobSummary(object):
                 self.getQueued() +
                 self.getRunning() +
                 self.getPending())
-    
+
     def getSuccess(self):
-        return self.jobStatus["success"] 
-        
+        return self.jobStatus["success"]
+
     def getFailure(self):
-        
-        return (self.jobStatus["failure"]["create"] + 
-                self.jobStatus["failure"]["submit"] + 
+
+        return (self.jobStatus["failure"]["create"] +
+                self.jobStatus["failure"]["submit"] +
                 self.jobStatus["failure"]["exception"])
-     
+
     def getSubmitted(self):
-        return (self.jobStatus["submitted"]["first"] + 
+        return (self.jobStatus["submitted"]["first"] +
                 self.jobStatus["submitted"]["retry"])
 
-    def getRunning(self): 
+    def getRunning(self):
         return self.jobStatus["submitted"]["running"];
-    
+
     def getPending(self):
         return self.jobStatus["submitted"]["pending"];
 
     def getCooloff(self):
-        return (self.jobStatus["cooloff"]["create"] + 
-                self.jobStatus["cooloff"]["submit"] + 
+        return (self.jobStatus["cooloff"]["create"] +
+                self.jobStatus["cooloff"]["submit"] +
                 self.jobStatus["cooloff"]["job"]);
 
     def getPaused(self):
-        return (self.jobStatus["paused"]["create"] + 
-                self.jobStatus["paused"]["submit"] + 
+        return (self.jobStatus["paused"]["create"] +
+                self.jobStatus["paused"]["submit"] +
                 self.jobStatus["paused"]["job"]);
-    
+
     def getQueued(self):
-        return (self.jobStatus["queued"]["first"] + 
+        return (self.jobStatus["queued"]["first"] +
                 self.jobStatus["queued"]["retry"])
-        
+
     def getJSONStatus(self):
         return {'sucess': self.getSuccess(),
                 'failure': self.getFailure(),
@@ -79,44 +79,44 @@ class JobSummary(object):
                 'queued': self.getQueued(),
                 'pending': self.getPending(),
                 'paused': self.getPaused(),
-                'created': self.getTotalJobs() 
+                'created': self.getTotalJobs()
                 }
 
 class ProgressSummary(object):
-    
+
     def __init__(self , progressReport = None):
         self.progress = {
                  "totalLumis": 0,
                  "events": 0,
                  "size": 0
          }
-        
+
         if progressReport != None:
             self.addProgressReport(progressReport)
-    
+
     def addProgressReport(self, progressReport):
-        
+
         #TODO need to validate the structure.
         for key in self.progress.keys():
             self.progress[key] += progressReport.get(key, 0)
-    
+
     def getReport(self):
         return self.progress
-            
+
 
 class RequestInfo(object):
-    
+
     def __init__(self, data):
         """
-        data structure is 
-        requestName ={'request_name1': 
+        data structure is
+        requestName ={'request_name1':
         {'agent_url1': {'status'
-        
+
         }
         """
         self.setData(data)
 
-        
+
     def setData(self, data):
         self.requestName = data['workflow']
         self.data = data
@@ -126,61 +126,63 @@ class RequestInfo(object):
             for agentUrl, agentRequestInfo in data['AgentJobInfo'].items():
                 self.jobSummary.addJobStatusInfo(agentRequestInfo.get('status', {}))
                 self.jobSummaryByAgent[agentUrl] = JobSummary(agentRequestInfo.get('status', {}))
-        
+
     def getJobSummary(self):
         return self.jobSummary
-    
+
     def getJobSummaryByAgent(self, agentUrl = None):
         if agentUrl:
             return self.jobSummaryByAgent[agentUrl]
         else:
             return self.jobSummaryByAgent
-    
+
     def getTotalTopLevelJobs(self):
         return self.data["total_jobs"]
-    
+
     def getTotalTopLevelJobsInWMBS(self):
         inWMBS = 0
         for agentRequestInfo in self.data["AgentJobInfo"].values():
             inWMBS += agentRequestInfo['status'].get('inWMBS', 0)
         return inWMBS
-    
+
     def getTotalInputLumis(self):
         return self.data.get("input_lumis", "N/A")
-    
+
     def getTotalInputEvents(self):
         return self.data["input_events"]
-    
+
     def getProgressSummaryByOutputDataset(self):
         """
         check sampleResult.json for datastructure
         """
         datasets = {};
+        try:
+            for agentRequestInfo in self.data["AgentJobInfo"].values():
+                tasks = agentRequestInfo["tasks"]
+                for task in tasks:
+                    for site in tasks[task].get("sites", []):
+                        for outputDS in tasks[task]["sites"][site].get("dataset", {}).keys():
+                            #TODO: need update the record instead of replacing.
+                            datasets.setdefault(outputDS, ProgressSummary())
+                            datasets[outputDS].addProgressReport(tasks[task]["sites"][site]["dataset"][outputDS])
+        except KeyError:
+            pass # Return empty list on KeyError of AgentJobInfo
 
-        for agentRequestInfo in self.data["AgentJobInfo"].values():
-            tasks = agentRequestInfo["tasks"]
-            for task in tasks:
-                for site in tasks[task].get("sites", []):
-                    for outputDS in tasks[task]["sites"][site].get("dataset", {}).keys():
-                        #TODO: need update the record instead of replacing.
-                        datasets.setdefault(outputDS, ProgressSummary())
-                        datasets[outputDS].addProgressReport(tasks[task]["sites"][site]["dataset"][outputDS])
-                        
         return datasets;
-     
+
 class RequestInfoCollection(object):
-    
+
     def __init__(self, data):
         self.collection = {}
         self.setData(data)
-        
+
     def setData(self, data):
         for requestName, requestInfo in data.items():
             self.collection[requestName] = RequestInfo(requestInfo)
-    
+
     def getData(self):
         return self.collection
-    
+
     def getJSONData(self):
         result = {}
         for requestInfo in self.collection.values():
@@ -188,4 +190,3 @@ class RequestInfoCollection(object):
             for agentUrl, jobSummary in requestInfo.getJobSummaryByAgent().items():
                 result[requestInfo.requestName][agentUrl]= jobSummary.getJSONStatus()
         return result
-    
